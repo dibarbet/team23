9a88cc3d390ad965a37ca158bf32a24a189d9fd7
dwright48 <dillon.wright@gatech.edu>
2014-09-05 15:51:25 -0400

    Update Person1.java

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -31,7 +31,7 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return input.substring(2, input.length()) + input.substring(0,2);
+	  return input.substring(2, input.length() - 1) + input.substring(0,2);
 	}
 	
 	/**
