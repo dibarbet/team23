966363526665e9cf421d17282db7ecb9cd91d2c9
Yaxiong <yliu710@gatech.edu>
2014-09-11 16:38:35 -0400

    M3 SpaceTrader

---
+++ M3/SpaceTrader/src/spacetrader/Player.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package spacetrader;
+
+/**
+ *
+ * @author YaxiongLiu
+ */
+public class Player {
+    // variable declariation
+    private String name;
+    private int pilot;
+    private int fighter;
+    private int trader;
+    private int engineer;
+    private int investor;
+    private int credit;
+    private Ship currShip;
+    
+    // constructor of Player, takes 5 argument
+    public Player(String newName, int newPilot, int newFighter, int newTrader, int newEngineer, int newInvestor) {
+        name = newName;
+        pilot = newPilot;
+        fighter = newFighter;
+        trader = newTrader;
+        engineer = newEngineer;
+        investor = newInvestor;
+        credit = 1000;
+        currShip = new Ship();
+    }
+}
---
+++ M3/SpaceTrader/src/spacetrader/SpaceTrader.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package spacetrader;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author YaxiongLiu
+ */
+public class SpaceTrader extends Application {
+    public Player player;
+    @Override
+    public void start(Stage primaryStage) {
+        new WelcomeWin().setVisible(true);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
