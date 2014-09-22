06c69684d587bfe1982d0763d1de0f681d4b831e
dwright48 <dillon.wright@gatech.edu>
2014-09-22 04:56:03 -0400

    Create Universe.java

---
+++ M3/SpaceTrader/src/spacetrader/Universe.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spacetrader;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Dillon
+ */
+public class Universe {
+    Random rand = new Random();
+    SolarSystem[] Universe = new SolarSystem[114];
+    String[] names = {
+        "Acamar",
+        "Adahn",		
+        "Aldea",
+        "Andevian",
+        "Antedi",
+        "Balosnee",
+        "Baratas",
+        "Brax",			
+        "Campor",
+        "Capelle",		
+        "Carzon",
+        "Castor",		
+        "Cestus",
+        "Cheron",		
+        "Courteney",	
+        "Daled",
+        "Damast",
+        "Davlos",
+        "Deneb",
+        "Deneva",
+        "Devidia",
+        "Draylon",
+        "Drema",
+        "Endor",
+        "Esmee",		
+        "Exo",
+        "Ferris",		
+        "Festen",		
+        "Fourmi",		
+        "Frolix",		
+        "Gemulon",
+        "Guinifer",		
+        "Hades",		
+        "Hamlet",		
+        "Helena",		
+        "Hulst",		
+        "Iodine",		
+        "Iralius",
+        "Janus",		
+        "Japori",
+        "Jarada",
+        "Jason",		
+        "Kaylon",
+        "Khefka",
+        "Kira",			
+        "Klaatu",		
+        "Klaestron",
+        "Korma",		
+        "Kravat",		
+        "Krios",
+        "Laertes",		
+        "Largo",
+        "Lave",			
+        "Ligon",
+        "Lowry",		
+        "Magrat",		
+        "Malcoria",
+        "Melina",
+        "Mentar",		
+        "Merik",
+        "Mintaka",
+        "Montor",		
+        "Mordan",
+        "Myrthe",		
+        "Nelvana",
+        "Nix",			
+        "Nyle",			
+        "Odet",
+        "Og",			
+        "Omega",		
+        "Omphalos",		
+        "Orias",
+        "Othello",		
+        "Parade",		
+        "Penthara",
+        "Picard",		
+        "Pollux",		
+        "Quator",
+        "Rakhar",
+        "Ran",			
+        "Regulas",
+        "Relva",
+        "Rhymus",
+        "Rochani",
+        "Rubicum",		
+        "Rutia",
+        "Sarpeidon",
+        "Sefalla",
+        "Seltrice",
+        "Sigma",
+        "Sol",			
+        "Somari",
+        "Stakoron",
+        "Styris",
+        "Talani",
+        "Tamus",
+        "Tantalos",		
+        "Tanuga",
+        "Tarchannen",
+        "Terosa",
+        "Thera",		
+        "Titan",		
+        "Torin",		
+        "Triacus",
+        "Turkana",
+        "Tyrus",
+        "Umberlee",		
+        "Utopia",		
+        "Vadera",
+        "Vagra",
+        "Vandor",
+        "Ventax",
+        "Xenon",
+        "Xerxes",		
+        "Yew",			
+        "Yojimbo",		
+        "Zalkon",
+        "Zuul"		
+    };
+    public Universe() {
+        for (int i = 0; i < 113; i++) {
+            String name = names[rand.nextInt(114)];
+            String[] n = new String[1];
+            n[0] = name;
+            Universe[i] = new SolarSystem(name, n, rand.nextInt(150), rand.nextInt(100));
+        }
+    }
+    
+    @Override
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < 113; i++) {
+            s += (Universe[i].getSolarName() + ": " + Universe[i].getX() + " " + Universe[i].getY() + ", ");
+        }
+        return s;
+    }
+    
+    public static void main(String[] args) {
+        Universe uni = new Universe();
+        System.out.println(uni.toString());
+    }
+}
