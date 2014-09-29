f249ea5b6275e2340717e4fffa42b2e980d49b00
dwright48 <dillon.wright@gatech.edu>
2014-09-29 19:29:39 -0400

    Update Ship.java
    Implemented methods to handle adding and removing cargo from the ship

--- M3/SpaceTrader/src/spacetrader/Ship.java
+++ M3/SpaceTrader/src/spacetrader/Ship.java
@@ -3,13 +3,41 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package spacetrader;
 
 /**
  *
- * @author YaxiongLiu
+ * @author Dillon
  */
 public class Ship {
-    //empty class, has not been implemented yet
+    private int count = 0;
+    private int limit = 30;
+    private String[] cargo = new String[limit];
+    
+    public void addCargo(int amount, String good) {
+        if (count + amount <= limit) {
+            for (int i = 0; i < amount; i++) {
+                cargo[count] = good;
+                count++;
+            }
+        }
+    }
+    
+    public void removeCargo(int amount, String good) {
+        if (count - amount >= 0) {
+            for (int i = 0; i < count; i++) {
+                if (cargo[i].equals(good)) {
+                    cargo[i] = null;
+                    count--;
+                    amount--;
+                }
+                if (amount == 0) {
+                    return;
+                }
+            }
+            if (amount > 0) {
+                System.out.print("Not enough of that good in cargo, sold all that was available.");
+            }
+        }
+    }
 }
