338ffdfc619019790b1a8bc4cd8dd23670b1d4ff
Yaxiong <yliu710@gatech.edu>
2014-10-17 23:30:15 -0400

    SpaceFX UI

---
+++ M3/SpaceFX/src/spacefx/MarketPlace/MarketPlaceController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spacefx.MarketPlace;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author YaxiongLiu
+ */
+public class MarketPlaceController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ M3/SpaceFX/src/spacefx/newgame/NewGameWinController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spacefx.newgame;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author YaxiongLiu
+ */
+public class NewGameWinController implements Initializable {
+   
+    
+    private void handleEmptyNameOKButtonAction(ActionEvent event) {
+        System.out.println("OK");
+    }
+    private void handleButtonAction2(ActionEvent event) {
+        
+    }
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
