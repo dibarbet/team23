230ae801c2731326721ef38bc42d2891cb2694f6
dwright48 <dillon.wright@gatech.edu>
2014-11-05 03:23:29 -0500

    Create Gnat.java

---
+++ M3/SpaceFX/src/spacefx/Gnat.java
@@ -0,0 +1,16 @@
+package spacefx;
+
+/**
+ *
+ * @author Dillon
+ */
+public class Gnat extends Ship {
+    
+    public Gnat() {
+        super(15, 13, 100, 10000, 1, 0, 1, 1);
+    }
+    
+    public String getShipType() {
+        return "Gnat";
+    }
+}
