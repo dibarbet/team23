50175b218043dabaaa885d94bd97099595fc50fb
dwright48 <dillon.wright@gatech.edu>
2014-11-05 03:14:06 -0500

    Fixed ship constructor and streamlined code

--- M3/SpaceFX/src/spacefx/Shipyard.java
+++ M3/SpaceFX/src/spacefx/Shipyard.java
@@ -17,43 +17,33 @@ public class Shipyard {
      * @return true if ship is bought, false otherwise
      */
     public boolean buyShip(String shipType) {
-        Ship s = new Ship(shipType);
-        Player p = GameData.getPlayer();
-        int price = getShipPrice(shipType);
-        System.out.println(price);
-        if ((p.getCredit() - price) < 0) {
+        Ship oldShip = GameData.getShip();
+        Ship newShip = oldShip.setShip(shipType);
+        Player player = GameData.getPlayer();
+        int price = newShip.getPrice();
+        if ((player.getCredit() - price) < 0) {
             System.out.println("Failed buy");
             return false;
         } else if (!checkTechLevel()) {
             return false;
         } else {
-            Ship oldShip = GameData.getShip();
-            s.nwater = oldShip.nwater;
-            s.nfur = oldShip.nfur;
-            s.nfood = oldShip.nfood;
-            s.nfire = oldShip.nfire;
-            s.nore = oldShip.nore;
-            s.ngame = oldShip.ngame;
-            s.nmed = oldShip.nmed;
-            s.nmach = oldShip.nmach;
-            s.nrob = oldShip.nrob;
-            s.nnarc = oldShip.nnarc;
-            s.cargo = oldShip.cargo;
-            p.loseCredit(price);
-            p.setShip(s);
-            GameData.setPlayer(p);
-            GameData.setShip(s);
+            newShip.nwater = oldShip.nwater;
+            newShip.nfur = oldShip.nfur;
+            newShip.nfood = oldShip.nfood;
+            newShip.nfire = oldShip.nfire;
+            newShip.nore = oldShip.nore;
+            newShip.ngame = oldShip.ngame;
+            newShip.nmed = oldShip.nmed;
+            newShip.nmach = oldShip.nmach;
+            newShip.nrob = oldShip.nrob;
+            newShip.nnarc = oldShip.nnarc;
+            newShip.cargo = oldShip.cargo;
+            player.loseCredit(price);
+            player.setShip(newShip);
+            GameData.setPlayer(player);
+            GameData.setShip(newShip);
             return true;
         }
-    }
-    public int getShipPrice(String shipType) {
-        int price = 0;
-        if (shipType.equals("Flea")) price = 2000;
-        if (shipType.equals("Gnat")) price = 10000;
-        if (shipType.equals("Firefly")) price = 25000;
-        if (shipType.equals("Mosquito")) price = 30000;
-        if (shipType.equals("Bumblebee")) price = 60000;
-        return price;
     }
     /**
      * Checks the tech level of the current solar system
