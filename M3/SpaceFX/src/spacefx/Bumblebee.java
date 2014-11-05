3575bbf2590401dd80a71e2a9baeab9530fddd7f
dwright48 <dillon.wright@gatech.edu>
2014-11-05 03:21:08 -0500

    Create Bumblebee.java

---
+++ M3/SpaceFX/src/spacefx/Bumblebee.java
@@ -0,0 +1,16 @@
+package spacetrader;
+
+/**
+ *
+ * @author Dillon
+ */
+public class Bumblebee extends Ship {
+    
+    public Bumblebee() {
+        super(25, 16, 100, 60000, 1, 2, 2, 2);
+    }
+    
+    public String getShipType() {
+        return "Bumblebee";
+    }
+}
